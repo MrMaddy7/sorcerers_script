762e1e21a6d46e0ceca93328b4dae80695bc7f2d
Shreemauli Raut <shreemauliraut7@gmail.com>
2023-07-22 19:27:19 +0530

    Added More Assets >> Fonts

--- src/features/navbar/NavbarView.jsx
+++ src/features/navbar/NavbarView.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Navbar.css";
 
 function NavbarView() {
-  return <nav></nav>;
+  return <nav className="NavbarView">
+    <img></img>
+    <img></img>
+    <h1></h1>
+    <input></input>
+    <img></img>
+    <img></img>
+  </nav>;
 }
 
 export default NavbarView;
