8c978fa0524043ce5a2a00917bd01bfda2cf3adf
Shreemauli Raut <shreemauliraut7@gmail.com>
2023-08-30 00:37:44 +0530

    Added PAGE NOT FOUND page into the routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,44 @@ import DashboardView from './features/dashBoard/DashboardView';
 import LoginView from "./features/login/LoginView";
 import SignUpView from "./features/signUp/SignUpView";
 import ModalView from "./features/modal/ModalView";
-import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
+import Lost from './assets/Lost.png';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 
 
 function App() {
 
+  //? Page not found component
+  const renderNotFound = () => {
+    const imageStyle = {
+      width: "50%",  // Adjust the width as needed
+      display: "block",
+      margin: "0 auto",
+    };
 
+    const textStyle = {
+      textAlign: "center",
+    };
+
+    return (
+      <div>
+        <img className="logo" src={ Lost } style={ imageStyle } alt="Lost Image" />
+        <h1 style={ textStyle }>Oh dear. Are you lost?</h1>
+      </div>
+    );
+  };
 
   return (
     <div className="App-layout">
-      <BackgroundMusic />
+      {/* <BackgroundMusic /> */ }
+      <ModalView />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<DashboardView />} />   
-          <Route path="/LoginView" element={<LoginView />} />
-          <Route path="/SignUpView" element={<SignUpView />} />   
+          <Route path="/Dashboard" element={ <DashboardView /> } />
+          <Route path="/Login" element={ <LoginView /> } />
+          <Route path="/SignUp" element={ <SignUpView /> } />
+          <Route path="*" element={ renderNotFound() } />
         </Routes>
       </BrowserRouter>
     </div>
